da12b76a87b22a6a6c58b66c10294f0bbaf2c890
chiemezie1 <chiemezieagbo1@gmail.com>
2022-07-01 02:59:10 -0700

    my work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 # include <stdio.h>
-
-int main(void) 
-{ puts("Programming is like building a multilingual puzzle\n"); 
+/**
+ * myfirst code on c
+ * will end with reture (0);
+ */
+int main(void) { puts("Programming is like building a multilingual puzzle\n"); 
 
 	return (0); }
 
