4ea29c2f4621a0777571078c83704cb6b893ad82
chiemezie1 <chiemezieagbo1@gmail.com>
2022-07-01 02:45:32 -0700

    my work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
-include <stdio.h>
+# include <stdio.h>
 
-/* my put out program*/
-int main(void) {
-       	puts("Programming is like building a multilingual puzzle\n"); 
-	return (0);
-}
-	
+/** main entry
+ *
+ * *returne (0);
+ *
+ * */
+
+int main(void) { puts("Programming is like building a multilingual puzzle\n"); 
+
+	return (0); }
+
+
