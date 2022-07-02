5d184e808dd1bb853febc46cdb33d53351d1c66b
Absolute <106808580+chiemezie1@users.noreply.github.com>
2022-07-03 00:14:29 +0100

    my work

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/** main - entry point
- *
- * return: 0 after printing the function
+/** heremy coment goes
+ * 
+ * this code will return 0
  */
 	int main(void)
 	{
-		printf("with proper grammar, but the outcome is a piece of art,\n);
-		returne 0;
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return 0;
 	}
