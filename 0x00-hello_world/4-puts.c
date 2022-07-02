9560355166d7af0661d20c97417b58bc74012d22
chiemezie1 <chiemezieagbo1@gmail.com>
2022-07-01 23:48:12 -0700

    my work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /** a program meant to putoutworeds
- * return 0 
+ *
+ * return 0
+ * 
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n);
+	puts("\"Programming is like building a multilingual puzzle\n");
 	reture 0;
 }
