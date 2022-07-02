151e78b9e30fccdda8b666f4931b1717de7b18e9
chiemezie1 <chiemezieagbo1@gmail.com>
2022-07-02 06:13:37 -0700

    my work

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,10 @@
-#include <stdio.h>                                                
-                                                                  
-/** main - entry point                                            
- *                                                                
- * return: 0 after printing the function                          
- *                                                                
- */                                                               
-        int main(void)                                            
-        {                                                         
-                        printf("with proper grammar, but the outco
-me is a piece of art,\n);                                         
-                        returne 0:                                
-        }                                                         
-~           
+#include <stdio.h>
+/** main - entry point
+ *
+ * return: 0 after printing the function
+ */
+	int main(void)
+	{
+		printf("with proper grammar, but the outcome is a piece of art,\n);
+		returne 0;
+	}
