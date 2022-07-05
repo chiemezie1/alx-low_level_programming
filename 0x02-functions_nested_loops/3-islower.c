f2aaaa3708721749f2c61a53f7695450cbb52d27
chiemezie1 <chiemezieagbo1@gmail.com>
2022-07-05 06:51:57 -0700

    my work

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stidio.h>
+/**
+ *main - check code for putout
+ *
+ *void funtion
+ *
+ *Return: Always 0.
+ */
+int _islower(int c);
+{	int ch;
+	for (ch >= 97 && ch <= 122)
+	{
+		return (1);
+	{
+	return (0);
+
+}
