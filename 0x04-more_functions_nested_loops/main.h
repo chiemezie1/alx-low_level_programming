4847784a04d6df4cbf1e5f4fa5930adf6e81744f
chiemezie1 <chiemezieagbo1@gmail.com>
2022-07-07 22:19:47 -0700

    my work

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,12 @@
-0-isupper.c
-1-isdigit.c
-2-mul.c
-3-print_numbers.c
-4-print_most_numbers.c
-5-more_numbers.c
-6-print_line.c
-7-print_diagonal.c
-8-print_square.c
-9-fizz_buzz.c
-10-print_triangle.c
-100-prime_factor.c
-101-print_number.c 
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
