b6075480fdac4b2b96b8a9ca752ea6c327a19c73
Absolute <106808580+chiemezie1@users.noreply.github.com>
2022-07-08 11:08:34 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
-  * mul - Multiplies two integers
-  * @x: The first number to multiply
-  * @y: The second number to multiply with first number
-  *
-  * Return: The result of multiplication between x param and y param
-  */
-int mul(int x, int y)
+ * mul - Multiples two integers.
+ * @a: The first integer to be multiplied.
+ *
+ * Return: The result of the multiplication.
+ */
+int mul(int a, int b)
 {
-	return (x * y);
+	return (a * b);
 }
