a327502361e4e42280c4f04e21f443135397c6b8
chiemezie1 <chiemezieagbo1@gmail.com>
2022-07-07 22:38:17 -0700

    my work

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
-  * _isupper - Check if a letter is upper
-  * @x: The number to be checked
-  *
-  * Return: 1 for upper letter or 0 for any else
-  */
-int _isupper(int c)
+ * main - check if a letter is an upper digit
+ *
+ * return: always 0.
+ */
+int _isdigit(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
-
 	return (0);
 }
