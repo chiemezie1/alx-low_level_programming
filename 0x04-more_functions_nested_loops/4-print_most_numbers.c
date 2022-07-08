76384c2f696c681a721f47f3c3b7a6159e4794a0
Absolute <106808580+chiemezie1@users.noreply.github.com>
2022-07-08 11:16:31 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,25 @@
-include "main.h"
+#include "main.h"
 /**
- * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
- */
+  * print_most_numbers - Print the numbers since 0 up to 9
+  *
+  *
+  * Return: The numbers since 0 up to 9
+  */
 void print_most_numbers(void)
 {
-	int num;
+	int c = 0;
 
-	for (num = 0; num <= 9; num++)
+	for (; c <= 9; c++)
 	{
-		if (num != 2 && num != 4)
-			_putchar((num % 10) + '0');
+		if (c == 2 || c == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(c + '0');
+		}
 	}
+
 	_putchar('\n');
 }
