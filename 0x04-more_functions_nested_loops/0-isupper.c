c4e4bfffc8fc7e3f5d5793cec1e066f9f811fa24
Absolute <106808580+chiemezie1@users.noreply.github.com>
2022-07-08 07:06:04 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,17 @@
 #include "main.h"
+
 /**
- * main - check if a letter is an upper digit
- *
- * return: always 0.
- */
-int _isdigit(int c)
+  * _isupper - Check if a letter is upper
+  * @x: The number to be checked
+  *
+  * Return: 1 for upper letter or 0 for any else
+  */
+int _isupper(int x)
 {
-	if (c >= 65 && c <= 90)
+	if (x >= 65 && x <= 90)
 	{
 		return (1);
 	}
+
 	return (0);
 }
