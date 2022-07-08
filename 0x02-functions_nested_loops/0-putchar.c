fb352733a5a734292fb7be7b017c925ea1cf9da9
Absolute <106808580+chiemezie1@users.noreply.github.com>
2022-07-08 02:28:20 +0100

    my work

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,13 @@
  */
 int main(void)
 {
-	_putchar("putchar");
-	_putchar("putchar");
-	_putchar("putchar");
+	_putchar(' ');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
