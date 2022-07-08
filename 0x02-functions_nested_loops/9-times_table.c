dcdf7887e605195a1743df1b20cc173c323c14b2
Absolute <106808580+chiemezie1@users.noreply.github.com>
2022-07-08 08:12:08 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,4 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
   * times_table - Prints the 9 times table, starting with 0
   *
@@ -8,13 +7,11 @@
 void times_table(void)
 {
 	int x, y, z, u, d;
-
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
 			z = x * y;
-
 			if (z > 9)
 			{
 				u = z % 10;
