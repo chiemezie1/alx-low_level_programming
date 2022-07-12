3e51bf937cc4638340e071772a9d5ab002b24b34
Absolute <106808580+chiemezie1@users.noreply.github.com>
2022-07-12 15:14:03 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,21 +6,27 @@
   * Return: void
   */
 void rev_string(char *s)
-{	int i, c, k;
-	char *a, aux;
+{
+	int i, c, k;
+	char *a, x;
+
 	a = s;
+
 	while (s[c] != '\0')
 	{
 		c++;
 	}
+
 	for (k = 1; k < c; k++)
 	{
 		a++;
 	}
+
 	for (i = 0; i < (c / 2); i++)
-	{	aux = s[i];
+	{
+		x = s[i];
 		s[i] = *a;
-		*a = aux;
+		*a = x;
 		a--;
 	}
 }
