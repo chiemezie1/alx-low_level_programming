eb0f44d8f796163aaadb7277313c437a61400c91
chiemezie1 <chiemezieagbo1@gmail.com>
2022-07-12 07:05:56 -0700

    my work

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * void rev_string(char *s)
+  * @s: The string to print
+  *
+  * Return: void
+  */
+void print_rev(char *s)
+{	char n;
+	n = rev(*s);
+	return (n);
+}
