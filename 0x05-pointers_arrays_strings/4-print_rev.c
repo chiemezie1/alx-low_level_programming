1cd2695b7ee28d96881389f8549bed6515710245
chiemezie1 <chiemezieagbo1@gmail.com>
2022-07-12 06:44:06 -0700

    my work

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - print a reverse of strings
+ *
+ * @*s is a charater
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	char n;
+	*s = n;
+	for (; n != '\0'; n++)
+	{
+		_putchat(n);
+	}
+	_putchar ("\n");
+}
