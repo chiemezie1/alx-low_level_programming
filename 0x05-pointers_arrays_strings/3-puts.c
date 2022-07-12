ae7a74fafc50193b2e86e97caa556038f1dc25a9
chiemezie1 <chiemezieagbo1@gmail.com>
2022-07-12 05:46:30 -0700

    my work

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen(char *s)
+ *
+ *@s:is a char which return the length
+ *
+ * Return: length
+ */
+int _strlen(char *s);
+{
+	char m;
+	m = strlen(*s);
+	return (m);
+}
