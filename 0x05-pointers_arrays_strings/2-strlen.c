c899ef51374b634917e747fa6e8b2576a9b80b88
chiemezie1 <chiemezieagbo1@gmail.com>
2022-07-12 06:01:12 -0700

    my work

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen(char *s)
+ *
+ *@s:is a char which return the length
+ *
+ * Return: length
+ */
+int _strlen(char *s);
+{
+	int n;
+	for (n = 0; n != '\0'; n++)
+	{
+		n++;
+		return (n);
+	}
+}
