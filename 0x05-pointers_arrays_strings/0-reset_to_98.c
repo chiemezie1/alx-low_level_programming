5fc1c4a41a3bbb393c914492c7d96a14a56df38e
chiemezie1 <chiemezieagbo1@gmail.com>
2022-07-12 04:38:22 -0700

    my work

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "stio.h"
+/**
+ * void -  reset_to_98(int *n)
+ *
+ *a fuction that is a pointer to an int
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	int *n;
+	*n = 98;
+	return (*n);
+
+}
