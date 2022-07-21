db9b43d8fa090200cec11b3d75e1ea07fe0022dd
Absolute <106808580+chiemezie1@users.noreply.github.com>
2022-07-21 10:11:40 +0100

    fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- *_puts_recursion - to print srings
- *
- *@s: strings of charater
- *
- *
- *Return: always 0.
- */
+  * _puts_recursion - Print a string followed by a new line
+  * @s: the string to print
+  *
+  * Return: always 0.
+  */
 void _puts_recursion(char *s)
-{	int i = 0;
-	int j = 0;
-	while (s < '\0')
-		i++;
-	for (j = 0; j <= i; j++)
+{
+	if (*s == '\0')
 	{
-		puts(s);
+		_putchar('\n');
+		return;
 	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
