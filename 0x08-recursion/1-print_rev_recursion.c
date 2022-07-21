891ec77d81887df9bf0af2d6486c7c4326445806
chiemezie1 <chiemezieagbo1@gmail.com>
2022-07-21 02:35:54 -0700

    main.h file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - fuction that prints in reverse
+ *
+ *@s: strings that is to be printed in revers
+ *
+ *
+ *Reture: Always 0.
+ *
+ */
+void _print_rev_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+
+	_print_rev_recursion(s);
+	s--;
+	_putchars(*s);
+
+}
