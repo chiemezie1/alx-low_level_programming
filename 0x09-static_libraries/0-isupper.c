a49d4ff5c5c2b922745e26e6918e8f11b7ac8c46
Absolute <106808580+chiemezie1@users.noreply.github.com>
2022-07-25 15:30:45 +0100

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,16 +1,17 @@
 #include "main.h"
+
 /**
- *main - funtion _isupper
- *
- *the charater putchar
- *
- *Return: Always 0.
- */
-int _isupper(int c)
+  * _isupper - Check if a letter is upper
+  * @x: The number to be checked
+  *
+  * Return: 1 for upper letter or 0 for any else
+  */
+int _isupper(int x)
 {
-	if (c >= 'A' && c <= 'Z')
-	}
+	if (x >= 65 && x <= 90)
+	{
 		return (1);
 	}
+
 	return (0);
 }
