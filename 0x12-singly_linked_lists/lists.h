f242582ba5560bc36350db2ebe5b138f7a21b9cb
Absolute <106808580+chiemezie1@users.noreply.github.com>
2022-08-12 13:51:08 +0100

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,6 @@
 #ifndef _LINKED_LIST
 #define _LINKED_LIST
 
-int _strlen(const char *s);
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -15,6 +8,7 @@ void free_list(list_t *head);
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
@@ -22,5 +16,12 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+int _strlen(const char *s);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
