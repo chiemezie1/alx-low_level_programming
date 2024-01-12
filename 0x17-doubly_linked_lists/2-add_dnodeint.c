bea679d48f344366e9d4644ed5b8617673fffc80
chiemezie1 <chiemezieagbo1@gmail.com>
2024-01-12 05:50:08 +0100

    2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @h: A pointer to the head of the list
+ *
+ * Return: The number of elements in the list
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+
+}
