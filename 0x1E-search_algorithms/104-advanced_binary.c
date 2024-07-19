829498aed5de35093f2e95b846b93836a78cb11e
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-19 04:41:06 +0100

    ./104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,21 +1,6 @@
 #include "search_algos.h"
 
 /**
- * print_array - Prints the current subarray being searched
- * @array: The array to be printed
- * @left: The starting index of the subarray
- * @right: The ending index of the subarray
- */
-void print_array(int *array, size_t left, size_t right)
-{
-    size_t i;
-
-    printf("Searching in array: ");
-    for (i = left; i <= right; i++)
-        printf("%d%s", array[i], i < right ? ", " : "\n");
-}
-
-/**
  * _advanced_binary - Recursively searches for a value in a sorted array
  * @array: The array to search in
  * @left: The left boundary of the subarray
@@ -29,8 +14,6 @@ int *_advanced_binary(int *array, size_t left, size_t right, int value)
 
     if (left > right)
         return (NULL);
-
-    print_array(array, left, right);
 
     mid = left + (right - left) / 2;
 
